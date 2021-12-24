80a7e0ba6e1bbdf1eb976bb7e02101f64f147014
ruifernandees <dev.ruifernandes@gmail.com>
2021-12-24 13:01:47 -0300

    feat(RepositoryTimeline): style and structure

--- src/pages/RepositoriesTimeline/index.tsx
+++ src/pages/RepositoriesTimeline/index.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import TimelineFragment from '../../components/TimelineFragment';
 import { GithubData } from '../../entities/GithubData';
+
+import './styles.css';
 
 const RepositoriesTimeline: React.FC = () => {
   const location = useLocation();
@@ -14,9 +17,68 @@ const RepositoriesTimeline: React.FC = () => {
     }
   }, []);
 
+  function formattedDate(date: Date) {
+    return date.toUTCString().substring(0, 16);
+  }
+
+  function formattedDescription(description: string) {
+    const descriptionAsArray = description.split(" ");
+    const maxCharacters = 80;
+    let totalOfCharacters = 0;
+    const limitedDescriptionArray = [];
+    for (const word of descriptionAsArray) {
+      const nextTotalOfCharacters = totalOfCharacters + word.length;
+      if (nextTotalOfCharacters > maxCharacters) break;
+      totalOfCharacters = nextTotalOfCharacters;
+      limitedDescriptionArray.push(word);
+    }
+    return limitedDescriptionArray.join(" ") + "...";
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen">
-      Hi, {githubData && githubData.user.name}
+    <div className="timeline-screen-container">
+      <header className='timeline-screen-header'>
+        <img 
+          src={githubData.user.avatarUrl} 
+          alt={`${githubData.user.name}'s image`}
+          className='user-image'
+        />
+        <h1 className="text-xl font-semibold">
+          Repository Timeline of {githubData.user.name}
+        </h1>
+      </header>
+      <ul className="timeline">
+        {
+          githubData.repositories.map((repository, index) => {
+            const isLeft = index % 2 !== 0; 
+
+            return (
+              <li 
+                className={`timeline-item ${isLeft ? 'left' : 'right'}`}
+                key={index}
+              >
+                <div className='complement'></div>
+                <TimelineFragment />
+                <a 
+                  className="repository-container" 
+                  href={repository.url}
+                  target="_blank"
+                >
+                  <p className="repository-name">{repository.name}</p>
+                  <hr className='repository-hr' />
+                  <p>{formattedDate(new Date(repository.createdAt))}</p>
+                  {
+                    repository.description &&
+                    <p className='repository-description'>
+                      {formattedDescription(repository.description)}
+                    </p>
+                  }
+                </a>
+              </li>
+            );
+          })
+        }
+      </ul>
     </div>
   );
 }
